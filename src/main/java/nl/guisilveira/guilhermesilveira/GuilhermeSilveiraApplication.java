144bbd50189669429b216e2b67790c57564000aa
Guilherme Silveira <guisilveirabatista@gmail.com>
2021-01-28 18:00:46 +0100

    Create initial project structure

---
+++ src/main/java/nl/guisilveira/guilhermesilveira/GuilhermeSilveiraApplication.java
@@ -0,0 +1,13 @@
+package nl.guisilveira.guilhermesilveira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuilhermeSilveiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuilhermeSilveiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/guisilveira/guilhermesilveira/GuilhermeSilveiraApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.guisilveira.guilhermesilveira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuilhermeSilveiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
