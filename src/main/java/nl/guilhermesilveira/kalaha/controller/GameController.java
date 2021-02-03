f6c4b8c98bbbe57468ce4e349d0bf20ef71dc019
Guilherme Silveira <guisilveirabatista@gmail.com>
2021-02-03 12:52:16 +0100

    Separated request actions to its fitting methods in GameController

--- src/main/java/nl/guilhermesilveira/kalaha/controller/GameController.java
+++ src/main/java/nl/guilhermesilveira/kalaha/controller/GameController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,14 +25,31 @@ public class GameController {
 	@Autowired
 	private GameService gameService;
 
+	@PostMapping
+	@Transactional
+	public ResponseEntity<?> newGame() {
+		GameDto gameDto = null;
+		try {
+			gameDto = this.gameService.newGame();
+			if (gameDto == null) {
+				return ResponseEntity.notFound().build();
+			}
+			return ResponseEntity.ok(gameDto);
+		} catch (GameException e) {
+			System.out.println(e.getMessage());
+			return ResponseEntity.status(400).body(e.getMessage());
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(500).build();
+		}
+	}
+
 	@GetMapping
-	public ResponseEntity<?> getGame(Long id) {
+	public ResponseEntity<?> loadGame(Long id) {
 		GameDto gameDto = null;
 		try {
 			if (id != null) {
 				gameDto = this.gameService.loadGame(id);
-			} else {
-				gameDto = this.gameService.newGame();
 			}
 			if (gameDto == null) {
 				return ResponseEntity.notFound().build();
@@ -46,12 +64,15 @@ public class GameController {
 		}
 	}
 
-	@PostMapping
+	@PutMapping
 	@Transactional
 	public ResponseEntity<?> makeMove(@RequestBody @Valid MoveForm moveForm) {
 		try {
 			MoveDto moveDto = moveForm.convertMoveFormToDto(moveForm);
 			GameDto gameDto = this.gameService.makeMove(moveDto);
+			if (gameDto == null) {
+				return ResponseEntity.notFound().build();
+			}
 			return ResponseEntity.ok(gameDto);
 		} catch (GameException e) {
 			System.out.println(e.getMessage());
