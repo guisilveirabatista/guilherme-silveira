47828ab1b7f815a9c3bcdb3a3249e37e1f66aa80
Guilherme Silveira <guisilveirabatista@gmail.com>
2021-02-03 15:21:13 +0100

    Added some more tests after refactor

--- src/test/java/nl/guilhermesilveira/kalaha/BoardTest.java
+++ src/test/java/nl/guilhermesilveira/kalaha/BoardTest.java
@@ -100,4 +100,77 @@ public class BoardTest {
 		assertEquals(true, kalahaPlayerRight.isKalaha());
 		assertEquals(Player.Player2.name(), kalahaPlayerRight.getPlayer().name());
 	}
+
+	@Test
+	void testCountStonesOnPlayerField() throws GameException {
+
+		int[] mockPits = { 6, 6, 6, 6, 6, 6, 0, 6, 6, 6, 6, 6, 6, 0 };
+
+		Game game = GameFactory.createGame(mockPits, GameStatus.PlayerLeftTurn);
+		game.setPits(BoardLogic.prepareBoard(game));
+
+		int stonesLeft = BoardLogic.countStonesOnPlayerField(game.getPlayerLeft(), game.getPits());
+		int stonesRight = BoardLogic.countStonesOnPlayerField(game.getPlayerRight(), game.getPits());
+
+		assertEquals(36, stonesLeft);
+		assertEquals(36, stonesRight);
+	}
+
+	@Test
+	void testCountAllStonesOnField() throws GameException {
+
+		int[] mockPits = { 6, 6, 6, 6, 6, 6, 0, 6, 6, 6, 6, 6, 6, 0 };
+
+		Game game = GameFactory.createGame(mockPits, GameStatus.PlayerLeftTurn);
+		game.setPits(BoardLogic.prepareBoard(game));
+
+		int allStones = BoardLogic.countAllStonesOnField(game.getPits());
+
+		assertEquals(72, allStones);
+	}
+
+	@Test
+	void testCountStonesOnPlayerKalaha() throws GameException {
+
+		int[] mockPits = { 6, 6, 6, 6, 6, 6, 0, 6, 6, 6, 6, 6, 6, 0 };
+
+		Game game = GameFactory.createGame(mockPits, GameStatus.PlayerLeftTurn);
+		game.setPits(BoardLogic.prepareBoard(game));
+
+		Pit playerLeftKalaha = BoardLogic.getPlayerKalaha(game.getPlayerLeft(), game.getPits());
+		Pit playerRightKalaha = BoardLogic.getPlayerKalaha(game.getPlayerRight(), game.getPits());
+
+		playerLeftKalaha.setStones(40);
+		playerRightKalaha.setStones(30);
+
+		int kalahaLeftStones = BoardLogic.countStonesOnPlayerKalaha(game.getPlayerLeft(), game.getPits());
+		int kalahaRightStones = BoardLogic.countStonesOnPlayerKalaha(game.getPlayerRight(), game.getPits());
+
+		assertEquals(40, kalahaLeftStones);
+		assertEquals(30, kalahaRightStones);
+	}
+
+	@Test
+	void grabAllStonesFromPlayerField() throws GameException {
+
+		int[] mockPits = { 6, 6, 6, 6, 6, 6, 0, 6, 6, 6, 6, 6, 6, 0 };
+
+		Game game = GameFactory.createGame(mockPits, GameStatus.PlayerLeftTurn);
+		game.setPits(BoardLogic.prepareBoard(game));
+
+		Pit playerLeftKalaha = BoardLogic.getPlayerKalaha(game.getPlayerLeft(), game.getPits());
+		Pit playerRightKalaha = BoardLogic.getPlayerKalaha(game.getPlayerRight(), game.getPits());
+
+		playerLeftKalaha.setStones(40);
+		playerRightKalaha.setStones(30);
+
+		BoardLogic.grabAllStonesFromPlayerField(game.getPlayerLeft(), game.getPits());
+		BoardLogic.grabAllStonesFromPlayerField(game.getPlayerRight(), game.getPits());
+
+		int stonesFieldLeft = BoardLogic.countStonesOnPlayerField(game.getPlayerLeft(), game.getPits());
+		int stonesFieldRight = BoardLogic.countStonesOnPlayerField(game.getPlayerLeft(), game.getPits());
+
+		assertEquals(0, stonesFieldLeft);
+		assertEquals(0, stonesFieldRight);
+	}
 }
